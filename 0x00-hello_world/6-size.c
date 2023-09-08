ce3e151c85d89b212d39221d0c6cbc8476787bf6
Fawazisa1994 <142639837+fawazisa1994@users.noreply.github.com>
2023-09-08 00:27:39 +0000

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print("Size of a char: %lu byte(s)\n", sizeof(char));
+	print("Size of an int: %lu byte(s)\n", sizeof(int));
+	print("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	print("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	print("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
+
